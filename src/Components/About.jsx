f527fd1a52e0075a6922bb8a5b7a60ad6c391288
Nafiuls <snafiul700@gmail.com>
2024-06-23 17:25:56 +0600

    added navbar hero and about section

---
+++ src/Components/About.jsx
@@ -0,0 +1,17 @@
+
+
+const About = () => {
+    return (
+        <div className="border-b border-neutral-900 pb-4 h-screen">
+            <div className="flex flex-col text-center items-center justify-center">
+                <h1 className="pb-16 text-4 xl font-thin tracking-tight lg:mt-16 lg:text-8xl uppercase">About <span className="text-neutral-500">Me</span></h1>
+                <p className="my-3 max-w-5xl font-light tracking-tighter">
+                    Hello! I'm Nafiul Siam, a dedicated and innovative Front-End Developer with a passion for transforming complex problems into intuitive and aesthetically pleasing digital solutions. With expertise in HTML, CSS, JavaScript, and modern frameworks like React and Tailwind CSS, I specialize in crafting sleek, responsive, and user-friendly web applications. My proficiency extends to using Chakra UI for customizable component libraries, Node.js and Express for building robust back-end services, and MongoDB and Firebase for efficient database management and real-time data handling. Additionally, I bring dynamic and engaging user experiences to life with Framer Motion. Throughout my academic journey, I have developed and honed my skills through various projects, applying best practices in coding, design, and user experience.I am eager to embark on a professional career and collaborate on exciting web development projects.
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default About
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,22 @@
+
+const Hero = () => {
+    return (
+        <div className="border-b h-screen border-neutral-900 pb-4 lg:mb-35">
+            <div className="flex flex-wrap">
+                <div className="w-full  ">
+                    <div className="flex flex-col items-center justify-center text-center">
+                        <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">NAFIUL SIAM
+                        </h1>
+                        <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight  text-transparent">
+                            FRONT-END DEVELOPER
+                        </span>
+                        <p className="my-3 max-w-3xl font-light tracking-tighter">Hello! I'm Nafiul Siam, a passionate and creative Front-End Developer with a strong background in crafting dynamic and responsive web applications. With a keen eye for design and a solid understanding of the latest web technologies, I strive to create engaging user experiences that are both visually appealing and highly functional.</p>
+                    </div>
+                </div>
+                {/* img */}
+            </div>
+        </div>
+    )
+}
+
+export default Hero
